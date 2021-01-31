7dee9caec46ac80423ea6196c5e2d67230642351
Sampath Surineni <sampath.skt@gmail.com>
2021-01-31 19:45:08 +0530

    added new url and reverted to basic database

--- django_web_hook/settings.py
+++ django_web_hook/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'django_web_hook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': POSTGRES_DB,
-        'USER': POSTGRES_USER,
-        'PASSWORD': POSTGRES_PASSWORD,
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
